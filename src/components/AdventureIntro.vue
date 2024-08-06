b27e23ae3229bff8383957f5af02672eb5e5dad3
prolixmagus <jeremypfishman@gmail.com>
2024-08-05 18:43:39 -0700

    Add styles and wrapper for sections

--- src/components/AdventureIntro.vue
+++ src/components/AdventureIntro.vue
@@ -9,10 +9,19 @@
 <template>
 	<section class="exposition">
 		<div class="inner-column">
-			<h1 class="attention-voice">{{ options.current.heading }}</h1>
-			<p>{{ options.current.text }}</p>
+			<div class="exposition-wrapper">
+				<h1 class="attention-voice">{{ options.current.heading }}</h1>
+				<p v-for="paragraph in options.current.paragraphs">{{ paragraph.text }}</p>
+			</div>
 		</div>
 	</section>
 </template>
 
-<style scoped></style>
+<style scoped>
+	.exposition-wrapper {
+		max-width: 900px;
+	}
+	p {
+		text-wrap: wrap;
+	}
+</style>
